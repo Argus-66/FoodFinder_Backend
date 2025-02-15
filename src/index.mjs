0a99dc9c43a4024f852a740617b24e6f67d5b7fc
Argus-66 <jqghebri@gmail.com>
2025-02-16 00:03:31 +0530

    commit

--- src/index.mjs
+++ src/index.mjs
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.set('trust proxy', 1);
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://food-finder-lime.vercel.app'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://food-finder-frontend-eh8g.vercel.app/'];
 
 app.use(cors({
     origin: function (origin, callback) {
